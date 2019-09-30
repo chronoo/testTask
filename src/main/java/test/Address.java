2666e85a3b940e7e7b03c56ef2c3ae9dc39ce711
CHRONO\chrono <chrono059@gmail.com>
2019-09-30 22:18:59 +0300

    Создан класс Address

---
+++ src/main/java/test/Address.java
@@ -0,0 +1,22 @@
+package test;
+
+public class Address {
+    private String street;
+    private Integer home;
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public Integer getHome() {
+        return home;
+    }
+
+    public void setHome(Integer home) {
+        this.home = home;
+    }
+}
