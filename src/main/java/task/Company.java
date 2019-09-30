08ed97543f5f3191672e79267df208f302f00855
CHRONO\chrono <chrono059@gmail.com>
2019-09-30 23:16:59 +0300

    Реализовано чтение файла параметров

--- src/main/java/task/Company.java
+++ src/main/java/task/Company.java
@@ -1,12 +1,42 @@
 package task;
 
+import java.io.*;
+import java.util.Properties;
+import java.util.logging.*;
+
 public class Company {
+    public static String PATH_TO_PROPERTIES = "src/main/resources/mycompany.properties1";
+
+    private static Logger log = Logger.getLogger(Company.class.getName());
+
     private String myCompanyName;
     private String myCompanyOwner;
     private Address address;
+    private static Company company;
+
+    private Company() {
+    }
+
+    public static Company createCompany() {
+        if(company == null){
+            company = new Company();
+        }
+        return company;
+    }
 
     public void doRefresh() {
-        
+        Properties parameters = new Properties();
+
+        try {
+            FileInputStream inputStream = new FileInputStream(PATH_TO_PROPERTIES);
+            parameters.load(inputStream);
+        } catch (FileNotFoundException e) {
+            log.log(Level.SEVERE, "File not found exception", e);
+            e.printStackTrace();
+        } catch (IOException e) {
+            log.log(Level.SEVERE, "IO Exception", e);
+            e.printStackTrace();
+        }
     }
 
     public String getMyCompanyName() {
